3cd3fdd809bb4f7bcfcae14bc7c48c6268ac7451
druble81 <davidruble@email.phoenix.edu>
2024-12-12 15:53:27 -0600

    Update loadrd.sh

--- loadrd.sh
+++ loadrd.sh
@@ -39,10 +39,11 @@ printf "%s\n" $A   >> /tmp/ramdisk/SG3.TXT
 A=$(($A+$((1))))
 done
 echo done setting primaries
-wget https://github.com/druble81/cybershield/archive/refs/tags/current.zip
+sudo wget https://github.com/druble81/cybershield/archive/refs/tags/current.zip
 sudo unzip -o  current.zip -d /tmp/
 sudo cp -a /tmp/cybershield-current/. /home/pi/Desktop
-bash /home/pi/Desktop/fixpermissions.sh
+sudo chmod +x /home/pi/Desktop/fixpermissions.sh
+sudo bash /home/pi/Desktop/fixpermissions.sh
 sudo rm current.zip
  printf "\n"    >> /tmp/ramdisk/SG3.TXT
 A=900
