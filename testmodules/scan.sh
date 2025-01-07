d3672cc938ebfdf140c5b6cd3dfbe0648f18a651
druble81 <davidruble@email.phoenix.edu>
2025-01-07 10:44:56 -0600

    Add files via upload

--- testmodules/scan.sh
+++ testmodules/scan.sh
@@ -1,16 +1,24 @@
 cd /home/pi/Desktop/testmodules
-BA=800
+BA=35
 while :
 do
 
 
 BA=$(($BA+1))
 
-if [  $BA -gt 4200  ]
+if [  $BA -gt 4400  ]
 then
-	BA=800
+	BA=35
 fi
 
-./adf43519 $BA 25000000 3
+./adf43519 $BA 25000000 1
+./adf43518 $BA 25000000 1
+./adf43517 $BA 25000000 1
+./adf43516 $BA 25000000 1
+./adf43515 $BA 25000000 1
+./adf43514 $BA 25000000 1
+./adf43513 $BA 25000000 1
+./adf43513 $BA 25000000 1
+./adf4351 $BA 25000000 1
 
 done
