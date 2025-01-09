e426c6a23bccf78d81b218cf4da6061ccddce194
druble81 <davidruble@email.phoenix.edu>
2025-01-09 13:42:15 -0600

    Add files via upload

---
+++ dfmodules/dtra.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+cd /home/-pi/Desktop
+
+
+RANDOM=$$
+
+while :
+do
+#RANDOM=$$
+sudo pkill -f t18.sh
+sudo pkill -f t19.sh
+sudo pkill -f t20.sh
+sudo pkill -f t21.sh
+sudo pkill -f adf4351
+
+
+
+A=$(($RANDOM % 4))
+#A=1
+
+echo $A
+echo $A
+echo $A
+echo $A
+echo $A
+
+if [[ $A == 0 ]]
+then
+bash  t18.sh&
+sleep $(($RANDOM % 4 + 2))
+fi
+
+if [[ $A == 1 ]]
+then
+bash  t19.sh&
+sleep $(($RANDOM % 3 + 3))
+fi
+
+if [[ $A == 2 ]]
+then
+bash  t20.sh&
+sleep $(($RANDOM % 5 + 2))
+fi
+
+if [[ $A == 3 ]]
+then
+bash  t21.sh&
+sleep $(($RANDOM % 5 + 2))
+fi
+
+
+done
+
+
+
+
+
