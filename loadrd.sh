f8d699b7391fa6b8535f50e77b41f0a97893cbcf
druble81 <davidruble@email.phoenix.edu>
2025-02-06 10:18:23 -0600

    Update loadrd.sh

--- loadrd.sh
+++ loadrd.sh
@@ -2,44 +2,44 @@
 sudo bash /home/pi/Desktop/ramdisk.sh
 cd /home/pi/Desktop
 bash fixpermissions.sh
-sudo cp 10k/adf4351 /tmp/ramdisk/adf43512s
-sudo cp 10k/adf43512 /tmp/ramdisk/adf435122
-sudo cp 10k/adf43513 /tmp/ramdisk/adf435132
-sudo cp 10k/adf43514 /tmp/ramdisk/adf435142
-sudo cp 10k/adf43515 /tmp/ramdisk/adf435152
-sudo cp 10k/adf43516 /tmp/ramdisk/adf435162
-sudo cp 10k/adf43517 /tmp/ramdisk/adf435172
-sudo cp 10k/adf43518 /tmp/ramdisk/adf435182
-sudo cp 10k/adf43519 /tmp/ramdisk/adf435192
-sudo cp alloffrd.sh /tmp/ramdisk/alloffrd.sh
-sudo cp /bin/pkill /tmp/ramdisk/pkill
+cp 10k/adf4351 /tmp/ramdisk/adf43512s
+cp 10k/adf43512 /tmp/ramdisk/adf435122
+cp 10k/adf43513 /tmp/ramdisk/adf435132
+cp 10k/adf43514 /tmp/ramdisk/adf435142
+cp 10k/adf43515 /tmp/ramdisk/adf435152
+cp 10k/adf43516 /tmp/ramdisk/adf435162
+cp 10k/adf43517 /tmp/ramdisk/adf435172
+cp 10k/adf43518 /tmp/ramdisk/adf435182
+cp 10k/adf43519 /tmp/ramdisk/adf435192
+cp alloffrd.sh /tmp/ramdisk/alloffrd.sh
+cp /bin/pkill /tmp/ramdisk/pkill
 
-sudo cp adf4351 /tmp/ramdisk/adf4351
-sudo cp adf43512 /tmp/ramdisk/adf43512
-sudo cp adf43513 /tmp/ramdisk/adf43513
-sudo cp adf43514 /tmp/ramdisk/adf43514
-sudo cp adf43515 /tmp/ramdisk/adf43515
-sudo cp adf43516 /tmp/ramdisk/adf43516
-sudo cp adf43517 /tmp/ramdisk/adf43517
-sudo cp adf43518 /tmp/ramdisk/adf43518
-sudo cp adf43519 /tmp/ramdisk/adf43519
-sudo cp loadrd.sh /tmp/ramdisk/loadrd.sh
+cp adf4351 /tmp/ramdisk/adf4351
+cp adf43512 /tmp/ramdisk/adf43512
+cp adf43513 /tmp/ramdisk/adf43513
+cp adf43514 /tmp/ramdisk/adf43514
+cp adf43515 /tmp/ramdisk/adf43515
+cp adf43516 /tmp/ramdisk/adf43516
+cp adf43517 /tmp/ramdisk/adf43517
+cp adf43518 /tmp/ramdisk/adf43518
+cp adf43519 /tmp/ramdisk/adf43519
+cp loadrd.sh /tmp/ramdisk/loadrd.sh
 
-sudo cat /dev/null > /tmp/ramdisk/SG3.TXT
+cat /dev/null > /tmp/ramdisk/SG3.TXT
 
 
 A=$1
 
 while [[ $A -lt $2 ]]
 do
-sudo printf "%s\n" $A   >> /tmp/ramdisk/SG3.TXT
+printf "%s\n" $A   >> /tmp/ramdisk/SG3.TXT
 
 
 
 A=$(($A+$((1))))
 echo done setting primaries
 done
-sudo  printf "\n"    >> /tmp/ramdisk/SG3.TXT
+printf "\n"    >> /tmp/ramdisk/SG3.TXT
 A=900
 exit
 
