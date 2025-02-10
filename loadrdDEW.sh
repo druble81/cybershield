7c9abc0101a35c005f4cffe693aa37a2ea1786f1
druble81 <davidruble@email.phoenix.edu>
2025-02-09 20:04:00 -0600

    Update loadrdDEW.sh

--- loadrdDEW.sh
+++ loadrdDEW.sh
@@ -30,19 +30,19 @@ cat /dev/null > /tmp/ramdisk/SG3.TXT
 
 A=35
 
-while [[ 35 -lt 162 ]]
+while [[ $A -lt 162 ]]
 do
 printf "%s\n" $A   >> /tmp/ramdisk/SG3.TXT
 A=$(($A+$((1))))
 done
 A=225
-while [[ 225 -lt 401 ]]
+while [[ $A -lt 401 ]]
 do
 printf "%s\n" $A   >> /tmp/ramdisk/SG3.TXT
 A=$(($A+$((1))))
 done
 A=600
-while [[ 600 -lt 1901 ]]
+while [[ $A -lt 1901 ]]
 do
 printf "%s\n" $A   >> /tmp/ramdisk/SG3.TXT
 A=$(($A+$((1))))
