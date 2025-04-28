5dbab473502705c9a326b33f8386d88652ed46ea
druble81 <davidruble@email.phoenix.edu>
2025-04-28 17:31:38 -0500

    Add files via upload

--- loadrd.sh
+++ loadrd.sh
@@ -74,7 +74,7 @@ do
     range=$((end - start + 1))
 
     # Calculate the divisor
-    divisor=$(( (range + 24) / 25 ))
+    divisor=$(( (range + 44) / 45 ))
 
  
 
@@ -139,7 +139,7 @@ else
     range=$((end - start + 1))
 
     # Calculate the divisor
-    divisor=$(( (range + 24) / 25 ))
+    divisor=$(( (range + 44) / 45 ))
 
     # Assign the result to a variable
     segment_size=$(( (range + divisor - 1) / divisor ))
