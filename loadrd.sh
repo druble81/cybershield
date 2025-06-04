c6be97bf26af2812d10e2f04ca99c53f7743bca6
druble81 <davidruble@email.phoenix.edu>
2025-06-04 12:36:44 -0500

    Add files via upload

--- loadrd.sh
+++ loadrd.sh
@@ -74,7 +74,7 @@ do
     range=$((end - start + 1))
 
     # Calculate the divisor
-    divisor=$(( (range + 999) / 1000 ))
+    divisor=$(( (range + 1499) / 1500 ))
 
  
 
@@ -139,7 +139,7 @@ else
     range=$((end - start + 1))
 
     # Calculate the divisor
-    divisor=$(( (range + 999) / 1000 ))
+    divisor=$(( (range + 1499) / 1500 ))
 
     # Assign the result to a variable
     segment_size=$(( (range + divisor - 1) / divisor ))
