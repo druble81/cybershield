2e850f7e6c93faedcd26097fb401f60fae68c54f
druble81 <davidruble@email.phoenix.edu>
2025-11-21 12:18:45 -0600

    Add files via upload

--- sa.sh
+++ sa.sh
@@ -36,10 +36,8 @@ else
     T6=$DEFAULT_T4
 fi
 
-
 T7=$((T5*2))
 T8=$((T6*2))
-
 
 cd /tmp/ramdisk
 
@@ -62,8 +60,43 @@ fi
 
 
 
-while :
-do
+# --- Define your three sets of numbers ---
+set1_t1=(300 400 500)
+set1_t2=(600 800 1000)
+
+set2_t1=(40 50 90)
+set2_t2=(80 100 120)
+
+set3_t1=(100 200 300)
+set3_t2=(200 400 1000)
+
+# Put all sets into arrays of arrays
+all_t1=(set1_t1[@] set2_t1[@] set3_t1[@])
+all_t2=(set1_t2[@] set2_t2[@] set3_t2[@])
+
+set_index=0   # tracks which set we are on
+cycle_count=0 # counts how many total loops run
+
+while true; do
+    # Pick the correct set
+    current_set_t1=("${!all_t1[$set_index]}")
+    current_set_t2=("${!all_t2[$set_index]}")
+
+    # Loop through numbers in the current set
+    for i in "${!current_set_t1[@]}"; do
+        t1="${current_set_t1[$i]}"
+        t2="${current_set_t2[$i]}"
+
+        echo "Set $((set_index+1)) | t1=$t1 | t2=$t2 | cycle=$cycle_count"
+
+        # --- Your logic here ---
+
+T3=$((T1*5))
+T4=$((T2*4))
+T5=$((T1))
+T6=$((T2))
+T7=$((T1*2))
+T8=$((T2*2))
 
 A=1
 #A=1
@@ -100,7 +133,8 @@ echo "......................N BURST MODE......................"
 echo "......................N BURST MODE......................"
 echo "......................N BURST MODE......................"
 
-sleep 1.$(($RANDOM % 3))
+#sleep 1.$(($RANDOM % 3))
+sleep 1
 
 sudo pkill -f adf4351
 
@@ -126,16 +160,21 @@ echo "......................10K BURST MODE......................$D"
 echo "......................10K BURST MODE......................$D"
 echo "......................10K BURST MODE......................$D"
 
-sleep 1.$(($RANDOM % 3))
+#sleep 1.$(($RANDOM % 3))
+sleep 1
 
 sudo pkill -f adf4351
 
 
 
+cycle_count=$((cycle_count+1))
+echo cycle count $cycle_count
 
-
-
-
-
-
+        # After 10 cycles, move to the next set
+        if (( cycle_count >= $(($RANDOM % 7 + 7)) )); then
+            set_index=$(( (set_index + 1) % 3 ))
+            echo "---- Switching to set $((set_index+1)) ----"
+		cycle_count=0
+        fi
+    done
 done
