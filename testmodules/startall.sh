65ddf1b6355951e568862247bafad013ad9af4ae
druble81 <davidruble@email.phoenix.edu>
2025-12-31 21:01:25 -0600

    Add files via upload

--- testmodules/startall.sh
+++ testmodules/startall.sh
@@ -1,27 +1,19 @@
-#!/bin/bash
 cd /home/pi/Desktop/testmodules
 
-echo "Starting 8-module hierarchical heterodyne cascade..."
+echo QUAD H.
 
-# --- Read SG3 base frequencies into array ---
-mapfile -t numbers < <(grep -Eo '[0-9]+' /tmp/ramdisk/SG3.TXT)
+# Function to load and shuffle numbers into an array
+load_and_shuffle_array() {
+    mapfile -t numbers < <(grep -v '^[[:space:]]*$' /tmp/ramdisk/SG3.TXT | shuf)
+    size=${#numbers[@]}
+    index=0
+}
 
-# Validate
-if [ "${#numbers[@]}" -eq 0 ]; then
-    echo "No numbers found in /tmp/ramdisk/SG3.TXT"
-    exit 1
-fi
+# Initial load and shuffle
+load_and_shuffle_array
 
-# --- Determine min/max ---
-MIN_BB="${numbers[0]}"
-MAX_BB="${numbers[${#numbers[@]}-1]}"
-
-# --- Timing / Control ---
-GROUP_STEP=25     # Frequency group step
-CURRENT_GROUP=$MIN_BB
-GROUP_DIR=1        # 1 = up, -1 = down
-GROUP_HOLD=$((RANDOM % 15 + 15))
-GROUP_COUNT=0
+SEED=$(od -An -N2 -i /dev/urandom)
+RANDOM=$SEED
 
 FILE="/home/pi/Desktop/power.txt"
 if [[ -f "$FILE" ]]; then
@@ -30,61 +22,47 @@ else
     C=2
 fi
 
-# --- Layered heterodyne offsets ---
-O1=1   # First layer ± offset
-O2=2   # Second layer ± offset
-O3=4   # Third layer ± offset
-JITTER=1  # optional ±1 Hz micro jitter
 
-# Infinite loop
-while :; do
-    # --- Group hold logic ---
-    GROUP_COUNT=$((GROUP_COUNT + 1))
-    if (( GROUP_COUNT >= GROUP_HOLD )); then
-        GROUP_COUNT=0
-        CURRENT_GROUP=$((CURRENT_GROUP + GROUP_DIR * GROUP_STEP))
+hz1="007000"
+hz2="000006"
+hz3="000020"
+hz4="000130"
 
-        # Reverse direction at bounds
-        if (( CURRENT_GROUP >= MAX_BB )); then
-            CURRENT_GROUP=$MAX_BB
-            GROUP_DIR=-1
-        elif (( CURRENT_GROUP <= MIN_BB )); then
-            CURRENT_GROUP=$MIN_BB
-            GROUP_DIR=1
-        fi
+echo QUAD H.
+
+while :
+do
+    # If index at end → reshuffle
+    if (( index >= size )); then
+        load_and_shuffle_array
     fi
 
-    # --- Base frequency within current group ---
-    BB=$(( CURRENT_GROUP + RANDOM % GROUP_STEP ))
-    echo "BB Group: $CURRENT_GROUP  |  BB: $BB"
+    BB=${numbers[$index]}
 
-    # --- Layer 1: Modules 1-2 ---
-    FREQ1=$(( BB + O1 + RANDOM % (2*JITTER+1) - JITTER ))
-    FREQ2=$(( BB - O1 + RANDOM % (2*JITTER+1) - JITTER ))
+    BB1=$((BB))
+    BB2=$((BB+1))
+    BB3=$((BB))
 
-    # --- Layer 2: Modules 3-6 ---
-    FREQ3=$(( FREQ1 + O2 + RANDOM % (2*JITTER+1) - JITTER ))
-    FREQ4=$(( FREQ1 - O2 + RANDOM % (2*JITTER+1) - JITTER ))
-    FREQ5=$(( FREQ2 + O2 + RANDOM % (2*JITTER+1) - JITTER ))
-    FREQ6=$(( FREQ2 - O2 + RANDOM % (2*JITTER+1) - JITTER ))
+    #echo "Random number selected: $BB"
+    #echo "Random number selected: $BB1"
+    #echo "Random number selected: $BB2"
+    #echo "Random number selected: $BB3"
 
-    # --- Layer 3: Modules 7-8 ---
-    FREQ7=$(( FREQ3 + O3 + RANDOM % (2*JITTER+1) - JITTER ))
-    FREQ8=$(( FREQ4 - O3 + RANDOM % (2*JITTER+1) - JITTER ))
+    
 
-    # --- Run modules (synchronously, wait for all) ---
-    ./adf4351  $BB       25000000 $C &
-    ./adf43512 $FREQ1   25000000 $C &
-    ./adf43513 $FREQ2   25000000 $C &
-    ./adf43514 $FREQ3   25000000 $C &
-    ./adf43515 $FREQ4   25000000 $C &
-    ./adf43516 $FREQ5   25000000 $C &
-    ./adf43517 $FREQ6   25000000 $C &
-    ./adf43518 $FREQ7   25000000 $C &
+    # ---- 8 MODULE COMMANDS ----
 
-    # Wait for all modules to complete update
-    wait
+    ./adf4351  $BB                  25000000 $C &
+    ./adf43512 $BB.$hz1          25000000 $C &
+    ./adf43513 $BB1.$hz2         25000000 $C &
+    ./adf43514 $BB1                 25000000 $C &
+    ./adf43515 $BB2                 25000000 $C &
+    ./adf43516 $BB2.$hz3         25000000 $C &
+    ./adf43517 $BB3.$hz4         25000000 $C &
+    ./adf43518 $BB3                 25000000 $C &
 
-    # --- Optional random sleep for timing jitter ---
-    sleep 0.00000$((RANDOM % 9 + 1))$((RANDOM % 9 + 1))$((RANDOM % 9 + 1))
+    # sleep after each block
+    sleep 0.00000$((RANDOM % 9+ 1))
+
+    ((index++))
 done
--- testmodules/testall.sh
+++ testmodules/testall.sh
@@ -1,54 +1,52 @@
-C=1
-sudo bash /home/pi/Desktop/alloffrd.sh
-sudo pkill -f "adf4351[0-9]*"
+C=2
 
 ./adf4351 500.5 25000000 $C
 ./adf4351 500.5 25000000 $C
 echo 1 on
 sleep 1
-./adf4351 off
+./adf4351
 echo 1 off
 sleep 1
 ./adf43512 500.5 25000000 $C
 ./adf43512 500.5 25000000 $C
 echo 2 on
 sleep 1
-./adf43512 off
+./adf43512
 echo 2 off
 sleep 1
 ./adf43513 500.5 25000000 $C
 ./adf43513 500.5 25000000 $C
 echo 3 on
 sleep 1
-./adf43513 off
+./adf43513
 echo 3 off
 sleep 1
 ./adf43514 500.5 25000000 $C
 ./adf43514 500.5 25000000 $C
 echo 4 on
 sleep 1
-./adf43514 off
+./adf43514
 echo 4 off
 sleep 1
 ./adf43515 500.5 25000000 $C
 ./adf43515 500.5 25000000 $C
 echo 5 on
 sleep 1
-./adf43515 off
+./adf43515
 echo 5 off
 sleep 1
 ./adf43516 500.5 25000000 $C
 ./adf43516 500.5 25000000 $C
 echo 6 on
 sleep 1
-./adf43516 off
+./adf43516
 echo 6 off
 sleep 1
 ./adf43517 500.5 25000000 $C
 ./adf43517 500.5 25000000 $C
 echo 7 on
 sleep 1
-./adf43517 off
+./adf43517
 echo 7 off
 sleep 1
 ./adf43518 500.5 25000000 $C
@@ -62,6 +60,6 @@ sleep 1
 ./adf43519 500.5 25000000 $C
 echo 9 on
 sleep 1
-./adf43519 off
+./adf43519
 echo 9 off
 sleep 1
