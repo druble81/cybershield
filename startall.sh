66a7b6c41bb4dc9d7bac405065114fdec02b4f86
druble81 <davidruble@email.phoenix.edu>
2026-08-16 14:42:39 -0500

    Add files via upload

--- startall.sh
+++ startall.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-
 cd /home/pi/Desktop
 
 RANDOM=$$
-
 
 # Path to the file
 FILE="/home/pi/Desktop/nvalues.txt"
@@ -22,10 +20,8 @@ else
     T2=$DEFAULT_T2
 fi
 
-
 T3=$((T1*2))
 T4=$((T2*2))
-
 
 /tmp/ramdisk/adf43513 1000 25000000 0 $T1 $T2&
 sudo pkill -f "adf4351[0-9]*"
@@ -40,13 +36,6 @@ else
     C=2
 fi
 
-
-
-
-
-
-
-
 /tmp/ramdisk/adf4351 1000 25000000 $C $T1 $T2&
 /tmp/ramdisk/adf43512 1000 25000000 $C $T1 $T2&
 /tmp/ramdisk/adf43513 1000 25000000 $C $T3 $T4&
@@ -57,14 +46,8 @@ fi
 /tmp/ramdisk/adf43518 1000 25000000 $C $T1 $T2&
 /tmp/ramdisk/adf43519 1000 25000000 $C $T1 $T2&
 
-
-
-
-
-
-
 echo "C is " $C
-sleep $(($RANDOM % 10 + 10))
+sleep $(($RANDOM % 5 + 5))
 sudo pkill -f "adf4351[0-9]*"
 
 /tmp/ramdisk/adf4351 1000 25000000 $C $T1 $T2&
@@ -77,25 +60,31 @@ sudo pkill -f "adf4351[0-9]*"
 /tmp/ramdisk/adf43518 1000 25000000 $C $T1 $T2&
 /tmp/ramdisk/adf43519 1000 25000000 $C $T1 $T2&
 
+# Initialize loop counter
+ITER=0
 
 while :
 do
+    ITER=$((ITER + 1))
+    sleep $(($RANDOM % 10 + 5))
 
-sleep $(($RANDOM % 10 + 5))
-#sudo pkill -f "adf4351[0-9]*"
+    # Every 20 iterations, trigger the loadrd command with a random parameter (15-50)
+    if (( ITER % 2 == 0 )); then
+        RAND_PARAM=$(( (RANDOM % 36) + 15 ))
+        echo ">>> [ITERATION $ITER] Triggering loadrd command with random parameter: $RAND_PARAM"
+        sudo /home/pi/Desktop/loadrd 35 4400 "$RAND_PARAM"
+    fi
 
-
-echo "......................NORMAL MODE......................$D"
-echo "......................NORMAL MODE......................$D"
-echo "......................NORMAL MODE......................$D"
-echo "......................NORMAL MODE......................$D"
-echo "......................NORMAL MODE......................$D"
-echo "......................NORMAL MODE......................$D"
-echo "......................NORMAL MODE......................$D"
-echo "......................NORMAL MODE......................$D"
-echo "......................NORMAL MODE......................$D"
-echo "......................NORMAL MODE......................$D"
-echo "......................NORMAL MODE......................$D"
+    echo "......................NORMAL MODE......................$D"
+    echo "......................NORMAL MODE......................$D"
+    echo "......................NORMAL MODE......................$D"
+    echo "......................NORMAL MODE......................$D"
+    echo "......................NORMAL MODE......................$D"
+    echo "......................NORMAL MODE......................$D"
+    echo "......................NORMAL MODE......................$D"
+    echo "......................NORMAL MODE......................$D"
+    echo "......................NORMAL MODE......................$D"
+    echo "......................NORMAL MODE......................$D"
+    echo "......................NORMAL MODE......................$D"
 
 done
-
